833482ec2356c9d8c6003026ef93ce3bf0171c27
endoritos <endysempai@gmail.com>
2024-03-14 09:14:41 +0100

    connection fail

---
+++ screens/Smart.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const Smart = () => {
+  return (
+    <View>
+      <Text>Welcome to ypur smart home app</Text>
+    </View>
+  )
+}
+
+export default Smart
+
+const styles = StyleSheet.create({})
